566d0698473e09c40d7cc2f7e6c47e9c627a8214
yousefelsherbieny <90617462+yousefelsherbieny@users.noreply.github.com>
2024-09-09 15:04:30 +0300

    work in language

--- angular-center/src/app/core/core.module.ts
+++ angular-center/src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { LOCALE_ID, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule, TitleCasePipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -13,6 +13,10 @@ import { ContactUsComponent } from './theme/contact-us/contact-us.component';
 import { BlogDetailComponent } from './theme/blog-detail/blog-detail.component';
 import { ServicePageComponent } from './theme/Services/service-page/service-page.component';
 import { ServicePageDetailComponent } from './theme/Services/service-page-detail/service-page-detail.component';
+import { TranslateModule } from '@ngx-translate/core';import 
+{ FixedService } from './utils/fixed.service';
+
+const fixed = new FixedService();
 
 @NgModule({
   declarations: [
@@ -30,13 +34,18 @@ import { ServicePageDetailComponent } from './theme/Services/service-page-detail
   imports: [
     CommonModule,  // For common Angular directives and pipes
     RouterModule,
-    TitleCasePipe  // For using routing functionality within this module
+    TitleCasePipe,
+    TranslateModule  // For using routing functionality within this module
   ],
   exports: [
     HeaderComponent, 
     FooterComponent, 
     LayoutComponent  // Exporting components to be used elsewhere in the app
-  ]
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'ar' },
+    { provide: FixedService, useValue: fixed },
+  ],
 })
 export class CoreModule {
   // Use forRoot() for singleton services or configurations that should be available globally
--- angular-center/src/app/core/theme/header/header.component.ts
+++ angular-center/src/app/core/theme/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FixedService } from '../../utils/fixed.service';
+import { GlobalService } from '../../utils/global.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(public fixed: FixedService, private global: GlobalService) {}
 
+  toggleLanguage() {
+    // Toggle between English and Arabic based on the current language
+    const nextLanguageCode = this.fixed.activeLang.code === 'en' ? 'ar' : 'en';
+    const nextLanguage = this.fixed.appLanguages.find((lang: { code: string; }) => lang.code === nextLanguageCode);
+    
+    if (nextLanguage) {
+      this.global.themeSettings(nextLanguage); // Call themeSettings with the next language
+    }
+  }
 }
