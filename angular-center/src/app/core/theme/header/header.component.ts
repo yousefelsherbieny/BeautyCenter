1dc9304f634b631dbed2f3928f9fb9dd1da9fc24
yousefelsherbieny <90617462+yousefelsherbieny@users.noreply.github.com>
2024-09-19 17:44:01 +0300

    work in dark mode

--- angular-center/src/app/core/theme/header/header.component.ts
+++ angular-center/src/app/core/theme/header/header.component.ts
@@ -5,10 +5,18 @@ import { GlobalService } from '../../utils/global.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  
+  isDarkMode = false;
+
   constructor(public fixed: FixedService, private global: GlobalService) {}
+
+  ngOnInit() {
+    this.isDarkMode = localStorage.getItem('darkMode') === 'true';
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+  }
 
   toggleLanguage() {
     // Toggle between English and Arabic based on the current language
@@ -18,5 +26,11 @@ export class HeaderComponent {
     if (nextLanguage) {
       this.global.themeSettings(nextLanguage); // Call themeSettings with the next language
     }
+  }
+  
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('darkMode', this.isDarkMode.toString());
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
   }
 }
